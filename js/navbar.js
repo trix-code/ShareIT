eea10d8737caa26d6a9865dc61889611b8c51adf
Tomáš Kotík <53795688+trix-code@users.noreply.github.com>
2024-11-29 14:26:18 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,13 @@
+function toggleMenu() {
+    const menuList = document.getElementById('menuList'); // Najde hlavní menu
+    const menuIcon = document.querySelector('.menu-icon'); // Najde hamburger ikonu
+
+    menuList.classList.toggle('show'); // Přepíná viditelnost menu
+    menuIcon.classList.toggle('open'); // Přepíná animaci hamburgeru
+}
+
+
+
 function toggleNotifications() {
     const dropdown = document.getElementById('notificationDropdown');
     dropdown.classList.toggle('hidden');
