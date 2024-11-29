da8564301284790186f8152a1ff8da1b458eb33f
Tomáš Kotík <53795688+trix-code@users.noreply.github.com>
2024-11-29 18:18:21 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -7,25 +7,3 @@ function toggleMenu() {
 }
 
 
-function updateNotificationIcon() {
-    fetch('../php/fetch_notifications.php')
-        .then(response => response.json())
-        .then(data => {
-            const notificationCount = data.notifications.filter(n => n.is_read === 0).length;
-            const notificationBadge = document.getElementById('notificationBadge');
-
-            if (notificationCount > 0) {
-                notificationBadge.textContent = notificationCount;
-                notificationBadge.classList.remove('hidden');
-            } else {
-                notificationBadge.classList.add('hidden');
-            }
-        })
-        .catch(error => {
-            console.error('Chyba při aktualizaci notifikační ikony:', error);
-        });
-}
-
-// Aktualizace notifikační ikony každých 30 sekund
-setInterval(updateNotificationIcon, 30000);
-updateNotificationIcon();
